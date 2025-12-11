9495f060cd8752903009f359abf1148e88074974
kanaya0000 <rx-7rr01@i.softbank.jp>
2025-12-12 05:33:59 +0900

    Add files via upload

--- js/button.js
+++ js/button.js
@@ -38,6 +38,36 @@ tap.forEach(tap_count => {
 })
 
 
+//プラスボタン設定
+const slide_btn=document.querySelector('.plus');
+
+slide_btn.addEventListener('click', () => {
+    console.log('クリックされました');
+    if (slide_btn.classList.contains('fil-off')) {
+        slide_btn.classList.remove('fil-off');
+        slide_btn.classList.add('fil-on');
+        //プラスボタンクリックでフィルター表示
+        const filter_on = document.querySelectorAll('.set-off');
+        filter_on.forEach(on => {
+            on.classList.remove('set-off');
+            on.classList.add('set-on');
+        }); 
+    } else {
+        slide_btn.classList.remove('fil-on');
+        slide_btn.classList.add('fil-off');
+
+        //プラスボタンクリックでフィルター非表示
+        const filter_off = document.querySelectorAll('.set-on');
+        filter_off.forEach(off => {
+            off.classList.remove('set-on');
+            off.classList.add('set-off');
+        });
+    }
+}, false);
+
+
+
+
 
 
 
--- js/scroll.js
+++ js/scroll.js
@@ -1,29 +1,62 @@
-        let class_list=[];
-        let height_list=[];
-        const windowHeight = window.innerHeight; //画面の高さを取得
-        const aa=document.querySelectorAll('.fadein');
+let class_list = [];
+let height_list = [];
+const windowHeight = window.innerHeight; //画面の高さを取得
+const aa = document.querySelectorAll('.fadein');
 
 
-        console.log(windowHeight / 2);
+console.log(windowHeight / 2);
 
 
-        //feadein
-        for(let i=0;i<aa.length;i++) {
-            class_list.push(aa[i]);
-            const bb=aa[i].getBoundingClientRect(); //TOPから要素までの高さ
-            height_list.push(bb.top);//サイト上部からの距離と画面サイズを引いた値をトリガーとする
+//feadein
+for (let i = 0; i < aa.length; i++) {
+    class_list.push(aa[i]);
+    const bb = aa[i].getBoundingClientRect(); //TOPから要素までの高さ
+    height_list.push(bb.top);//サイト上部からの距離と画面サイズを引いた値をトリガーとする
+}
+
+console.log(class_list);
+
+window.addEventListener('scroll', () => {
+    // console.log(window.scrollY);　スクロール量
+
+    for (let j = 0; j < class_list.length; j++) {
+        if (window.scrollY > height_list[j] - windowHeight) {
+            class_list[j].classList.add('on');
+        } else {
+            class_list[j].classList.remove('on');
         }
+    }
+}, false)
 
-        console.log(class_list);
 
-        window.addEventListener('scroll',()=> {
-            // console.log(window.scrollY);　スクロール量
+//slideの表示
+const slide = document.querySelectorAll('.slide');
+let list1 = [];
+let list2 = [];
 
-            for(let j=0;j<class_list.length;j++) {
-                if(window.scrollY > height_list[j]-windowHeight) {
-                    class_list[j].classList.add('on');
-                } else {
-                    class_list[j].classList.remove('on');
-                }
-            }
-        },false)
+// for (let i = 0; i < slide.length; i++) {
+//     list1.push(slide[i]);
+//     const bb = slide[i].getBoundingClientRect(); //TOPから要素までの高さ
+//     list2.push(bb.top);//サイト上部からの距離と画面サイズを引いた値をトリガーとする
+// }
+
+window.addEventListener('scroll', () => {
+    // for (let j = 0; j < class_list.length; j++) {
+        // if (window.scrollY > 1000) {
+        //     list1[0].classList.add('on');
+        //     list1[1].classList.add('on');
+        // } else {
+        //     list1[0].classList.remove('on');
+        //     list1[1].classList.remove('on');
+        // }
+    // }
+        if (window.scrollY > 1000) {
+        slide[0].classList.add('on');
+        slide[1].classList.add('on');
+    } else {
+        slide[0].classList.remove('on');
+        slide[1].classList.remove('on');
+    }
+}, false)
+
+// }, false)
