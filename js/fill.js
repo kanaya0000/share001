0bb8c290dfae16ad1c095347e335b953958269dc
kanaya0000 <rx-7rr01@i.softbank.jp>
2025-12-24 05:05:53 +0900

    Add files via upload

--- js/fill.js
+++ js/fill.js
@@ -6,7 +6,7 @@ let selectedArea = null;
 function filterItems() {
     console.log('selectedCategory::' + selectedCategory);
     console.log('selectedArea::' + selectedArea);
-    items.forEach(item => {
+    items.forEach((item, index) => {
         //判定　ブール値 
         // const isCategoryMatch = !selectedCategory || item.classList.contains(selectedCategory);
         //true_選択 false_「全て」を選択
@@ -27,13 +27,26 @@ function filterItems() {
         // console.log(isAreatext + '^^^ジャンルtext^^^');
         // console.log(selectedCategory);
 
-
+        if (index === 1) {
+            // console.log(index + '番目');
+            // const title=document.querySelectorAll('section-top');
+            console.log(title.id + 'タイトル');
+            for (let i = 0; i < id_list.length; i++) {
+                const title = document.getElementById(id_list[i]);
+                title.style.display = 'none';
+            }
+        }
         if (!isCategoryMatch && !isAreaMatch) {
             //どちらも「全て」選択
             console.log('全て選択');
             item.style.display = 'flex';
+            //タイトルの再表示
+            for (let j = 1; j < 4; j++) {
+                const judge = document.getElementById(id_list[j]);
+                judge.style.display = 'block';
+            }
             return;
-        }else if(isAreatext && isCategorytext) {
+        } else if (isAreatext && isCategorytext) {
             console.log('エリアとジャンル選択');
             item.style.display = 'flex';
             // console.log(item);
@@ -44,14 +57,38 @@ function filterItems() {
             // console.log(item);
             return;
 
-        } else if(isCategorytext && !isAreaMatch) {
+        } else if (isCategorytext && !isAreaMatch) {
             console.log('エリアのみ選択');
             item.style.display = 'flex';
             // console.log(item);
             return;
-        }  else {
+        } else {
             // console.log('非表示');
             item.style.display = 'none';
+        }
+
+        //ヒットしたお店の地区タイトル再表示
+        for (let i = 0; i < check_list.length; i++) {
+            if (selectedCategory == check_list[i]) {
+                // console.log('----i----' + i);
+
+                if (1 <= i && i <= 8) {
+                    const judge = document.getElementById(id_list[1]);
+                    judge.style.display = 'block';
+                } else if (9 <= i && i <= 11) {
+                    console.log('中濃エリア');
+                    const judge = document.getElementById(id_list[0]);
+                    judge.style.display = 'block';
+                } else if (12 <= i && i <= 17) {
+                    console.log('西濃エリア');
+                    const judge = document.getElementById(id_list[2]);
+                    judge.style.display = 'block';
+                } else {
+                    console.log('愛知エリア');
+                    const judge = document.getElementById(id_list[3]);
+                    judge.style.display = 'block';
+                }
+            }
         }
 
 
--- js/js/script.js
+++ js/js/script.js
@@ -1,6 +1,16 @@
-$(function() { ('.slider').slick({
-    autoplay:true,
-    autoplaySpeed:5000,
-    dots:true,
-});
+// $(function() { ('.slider').slick({
+//     autoplay:true,
+//     autoplaySpeed:5000,
+//     dots:true,
+// });
+// })
+
+
+$(function () {
+    $('.slide_box').slick({
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true
+    });
 })
